366fe6f3dc432e14e3f04862ad9da9da110634f1
Kévin ANGELE <kevin.angele.pro@gmail.com>
2018-08-20 18:26:40 +0200

    CodinGame (java) - 'The power of Thor - Episode 1' (training/easy) - Résolution

--- training/easy/power-of-thor-episode-1/code.java
+++ training/easy/power-of-thor-episode-1/code.java
@@ -12,21 +12,69 @@ class Player {
 
     public static void main(String args[]) {
         Scanner in = new Scanner(System.in);
-        int lightX = in.nextInt(); // the X position of the light of power
-        int lightY = in.nextInt(); // the Y position of the light of power
-        int initialTX = in.nextInt(); // Thor's starting X position
-        int initialTY = in.nextInt(); // Thor's starting Y position
+        int lightX = in.nextInt();      // the X position of the light of power
+        int lightY = in.nextInt();      // the Y position of the light of power
+        int initialTX = in.nextInt();   // Thor's starting X position
+        int initialTY = in.nextInt();   // Thor's starting Y position
 
         // game loop
         while (true) {
             int remainingTurns = in.nextInt(); // The remaining amount of turns Thor can move. Do not remove this line.
 
+            //Message de DEBUG
+            System.err.println("Remaining turns = " + remainingTurns);
+
             // Write an action using System.out.println()
             // To debug: System.err.println("Debug messages...");
 
+            String moveX = "";                      //Direction and number of steps toward X axis
+            String moveY = "";                      //Direction and number of steps toward Y axis
 
-            // A single line providing the move to be made: N NE E SE S SW W or NW
-            System.out.println("SE");
+            //*******************
+            //* X axis movement *
+            //*******************
+
+            int diffX = initialTX - lightX;         // Calculate the difference between Thor and Light position
+
+            System.err.println("Différence sur X = " + diffX);
+
+            if ( (diffX < 0) &&
+                    (initialTX < 39) ) {            // Don't move out of the plan
+                moveX = "E";                        // Move East
+                initialTX++;
+            }
+
+            if ( (diffX > 0) &&
+                    (initialTX > 0) ) {
+                moveX = "W";                        // Move West
+                initialTX--;
+            }
+
+            System.err.println("  ==> " + moveX);
+
+            //*******************
+            //* Y axis movement *
+            //*******************
+
+
+            int diffY = initialTY - lightY;         // Calculate the difference between Thor and Light position
+            System.err.println("Différence sur Y = " + diffY);
+            if ( (diffY < 0) &&
+                    (initialTY < 17) ) {            // Don't move out of the plan
+                moveY = "S";                        // Move South
+                initialTY++;
+            }
+
+            if ( (diffY > 0) &&
+                    (initialTY > 0) ) {
+                moveY = "N";                        // Move North
+                initialTY--;
+            }
+
+            System.err.println("  ==> " + moveY);
+
+            // A single line providing the move to be made: N, NE, E, SE, S, SW, W or NW
+            System.out.println(moveY + moveX);
         }
     }
 }
